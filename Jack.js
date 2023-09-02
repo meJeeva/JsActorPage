afafb6a1e4c84c5ff523dcaea768cb0bb0c5982f
meJeeva <jeevaofficialjob@gmail.com>
2023-09-02 07:43:35 +0530

    Added paragraph

--- Jack.js
+++ Jack.js
@@ -1,3 +1,12 @@
 const Jack = () => {
   return 'Hello Jack'
 }
+
+
+const aboutJack = () => {
+  return `Captain Jack Sparrow is a fictional character and the main protagonist of the Pirates of the Caribbean film series and franchise. The character was created by screenwriters Ted Elliott and Terry Rossio and is portrayed by Johnny Depp.
+
+  The characterization of Sparrow is based on a combination of The Rolling Stones' guitarist Keith Richards and Looney Tunes cartoons, specifically the characters Bugs Bunny and Pepé Le Pew. He first appears in the 2003 film Pirates of the Caribbean: The Curse of the Black Pearl. He later appears in the sequels Dead Man's Chest (2006), At World's End (2007), On Stranger Tides (2011), and Dead Men Tell No Tales (2017).
+  
+  In the films, Sparrow is one of the nine pirate lords in the Brethren C`
+}
